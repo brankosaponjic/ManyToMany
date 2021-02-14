fe0190177cf123f602459daec0528474b47f7494
Branko <brankosaponjic76@gmail.com>
2021-02-14 12:47:59 +0100

    With @Transactional now we can get the projects also.

--- src/test/java/brankosaponjic/manytomany/ManytomanyApplicationTests.java
+++ src/test/java/brankosaponjic/manytomany/ManytomanyApplicationTests.java
@@ -5,6 +5,7 @@ import brankosaponjic.manytomany.repository.ProgrammerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,10 +16,12 @@ class ManytomanyApplicationTests {
     ProgrammerRepository programmerRepository;
 
     @Test
+    @Transactional
     void testFindProgrammer() {
         Optional<Programmer> programmer = programmerRepository.findById(1);
         if (programmer.isPresent()) {
             System.out.println(programmer);
+            System.out.println(programmer.get().getProjects());
         }
     }
 
